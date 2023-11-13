4b2ede4a2faa9922732742faeeae0275d1521ab3
farhanjamil1 <150520713+farhanjamil1@users.noreply.github.com>
2023-11-13 21:18:51 +0500

    Add files via upload
    Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ project5.js
@@ -0,0 +1,3 @@
+var faimous_person = "Fatima(RA)";
+var massage = faimous_person + 'Once said, "A womens best jewellery is her shyness."';
+console.log(massage);
---
+++ project5.ts
@@ -0,0 +1,4 @@
+let faimous_person: string = "Fatima(RA)";
+let massage :string = faimous_person + 'Once said, "A womens best jewellery is her shyness."';
+
+console.log (massage)
